34e11b53c58ec96e9122042f4a9dbaca2365219b
OldHawk <taobataoma@gmail.com>
2017-03-26 18:27:31 +0800

    add topbar menu: torrents,forums,chat,upload,rules,ranking,vip etc. those menu item also added translate

--- modules/articles/client/config/articles.client.menus.js
+++ modules/articles/client/config/articles.client.menus.js
@@ -8,20 +8,19 @@
   menuConfig.$inject = ['menuService'];
 
   function menuConfig(menuService) {
-    menuService.addMenuItem('topbar', {
-      title: 'Articles',
-      state: 'articles',
-      type: 'dropdown',
-      roles: ['*'],
-      position: 20
-
-    });
-
-    // Add the dropdown list item
-    menuService.addSubMenuItem('topbar', 'articles', {
-      title: 'List Articles',
-      state: 'articles.list',
-      roles: ['*']
-    });
+    //menuService.addMenuItem('topbar', {
+    //  title: 'Articles',
+    //  state: 'articles',
+    //  type: 'dropdown',
+    //  roles: ['*'],
+    //  position: 20
+    //});
+    //
+    //// Add the dropdown list item
+    //menuService.addSubMenuItem('topbar', 'articles', {
+    //  title: 'List Articles',
+    //  state: 'articles.list',
+    //  roles: ['*']
+    //});
   }
 }());
