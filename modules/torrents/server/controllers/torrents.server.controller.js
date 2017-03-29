f8144d943414ae4b190a3cd58875fbd4cb27105a
OldHawk <taobataoma@gmail.com>
2017-03-29 11:04:54 +0800

    add torrents file upload API

--- modules/torrents/server/controllers/torrents.server.controller.js
+++ modules/torrents/server/controllers/torrents.server.controller.js
@@ -4,8 +4,13 @@
  * Module dependencies
  */
 var path = require('path'),
+  config = require(path.resolve('./config/config')),
   mongoose = require('mongoose'),
   errorHandler = require(path.resolve('./modules/core/server/controllers/errors.server.controller')),
+  multer = require('multer'),
+  User = mongoose.model('User'),
+  fs = require('fs'),
+  validator = require('validator'),
   tmdb = require('moviedb')('7888f0042a366f63289ff571b68b7ce0');
 
 /**
@@ -21,5 +26,50 @@ exports.movieinfo = function (req, res) {
       res.json(info);
     }
   });
+};
+
+exports.upload = function (req, res) {
+  var user = req.user;
+  //var existingFileUrl;
+
+  // Filtering to upload only images
+  var multerConfig = config.uploads.torrent.file;
+  multerConfig.fileFilter = require(path.resolve('./config/lib/multer')).torrentFileFilter;
+  var upload = multer(multerConfig).single('newTorrentFile');
+
+  if (user) {
+    //existingFileUrl = user.profileImageURL;
+    uploadFile()
+      .then(function () {
+        res.json(user);
+      })
+      .catch(function (err) {
+        res.status(422).send(err);
+      });
+  } else {
+    res.status(401).send({
+      message: 'User is not signed in'
+    });
+  }
+
+  function uploadFile() {
+    return new Promise(function (resolve, reject) {
+      upload(req, res, function (uploadError) {
+        if (uploadError) {
+          var message = errorHandler.getErrorMessage(uploadError);
+
+          if (uploadError.code === 'LIMIT_FILE_SIZE') {
+            message = 'Torrent file too large. Maximum size allowed is ' + (config.uploads.torrent.file.limits.fileSize / (1024 * 1024)).toFixed(2) + ' Mb files.';
+          }
+
+          //reject(errorHandler.getErrorMessage(uploadError));
+          reject(message);
+        } else {
+          resolve();
+        }
+      });
+    });
+  }
+
 };
 
--- modules/torrents/server/routes/torrents.server.routes.js
+++ modules/torrents/server/routes/torrents.server.routes.js
@@ -10,4 +10,7 @@ module.exports = function (app) {
   app.route('/api/movieinfo')
     .get(torrents.movieinfo);
 
+  app.route('/api/torrents/upload')
+    .post(torrents.upload);
+
 };
