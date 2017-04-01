e2ec5b7d0139e47593596c4dd79a9f1d1554ee00
OldHawk <taobataoma@gmail.com>
2017-04-01 10:28:54 +0800

    add the footer view

--- modules/core/client/directives/page-title.client.directive.js
+++ modules/core/client/directives/page-title.client.directive.js
@@ -18,7 +18,7 @@
       $rootScope.$on('$stateChangeSuccess', listener);
 
       function listener(event, toState) {
-        var applicationCoreTitle = 'meanTorrent',
+        var applicationCoreTitle = 'CHD.im',
           separeteBy = ' - ';
         if (toState.data && toState.data.pageTitle) {
           var stateTitle = $interpolate(toState.data.pageTitle)($state.$current.locals.globals);
