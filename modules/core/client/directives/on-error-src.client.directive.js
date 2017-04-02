546e06b701282dc1ba25d9e9d31f92b8f49c9ab7
OldHawk <taobataoma@gmail.com>
2017-04-02 18:05:07 +0800

    add directive onErrorSrc when ng-src return 404 to load a default img

---
+++ modules/core/client/directives/on-error-src.client.directive.js
@@ -0,0 +1,25 @@
+(function () {
+  'use strict';
+
+  // Focus the element on page load
+  // Unless the user is on a small device, because this could obscure the page with a keyboard
+
+  angular.module('core')
+    .directive('onErrorSrc', onErrorSrc);
+
+  function onErrorSrc() {
+    var directive = {
+      link: link
+    };
+
+    return directive;
+
+    function link(scope, element, attrs) {
+      element.bind('error', function () {
+        if (attrs.src !== attrs.onErrorSrc) {
+          attrs.$set('src', attrs.onErrorSrc);
+        }
+      });
+    }
+  }
+}());
