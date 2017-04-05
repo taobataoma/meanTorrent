33b2a23bd21f9190393e075e6a843202ebef2224
OldHawk <taobataoma@gmail.com>
2017-04-05 12:35:03 +0800

    add filter bytes to format torrent size

---
+++ modules/core/client/filter/bytes.client.filter.js
@@ -0,0 +1,19 @@
+(function () {
+  'use strict';
+
+  // Focus the element on page load
+  // Unless the user is on a small device, because this could obscure the page with a keyboard
+
+  angular.module('core')
+    .filter('bytes', bytes);
+
+  function bytes() {
+    return function (bytes, precision) {
+      if (bytes === 0 || isNaN(parseFloat(bytes)) || !isFinite(bytes)) return '-';
+      if (typeof precision === 'undefined') precision = 1;
+      var units = ['bytes', 'kB', 'MB', 'GB', 'TB', 'PB'],
+        number = Math.floor(Math.log(bytes) / Math.log(1024));
+      return (bytes / Math.pow(1024, Math.floor(number))).toFixed(precision) + ' ' + units[number];
+    };
+  }
+}());
