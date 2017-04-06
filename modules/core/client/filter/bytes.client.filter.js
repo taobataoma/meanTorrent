48da324e3ea22df563274b550f4c750b75a19f70
OldHawk <taobataoma@gmail.com>
2017-04-06 09:45:20 +0800

    change bytes filter return format

--- modules/core/client/filter/bytes.client.filter.js
+++ modules/core/client/filter/bytes.client.filter.js
@@ -11,9 +11,10 @@
     return function (bytes, precision) {
       if (bytes === 0 || isNaN(parseFloat(bytes)) || !isFinite(bytes)) return '-';
       if (typeof precision === 'undefined') precision = 1;
-      var units = ['bytes', 'kB', 'MB', 'GB', 'TB', 'PB'],
+      //var units = ['bytes', 'kB', 'MB', 'GB', 'TB', 'PB'],
+      var units = ['b', 'K', 'M', 'G', 'T', 'P'],
         number = Math.floor(Math.log(bytes) / Math.log(1024));
-      return (bytes / Math.pow(1024, Math.floor(number))).toFixed(precision) + ' ' + units[number];
+      return (bytes / Math.pow(1024, Math.floor(number))).toFixed(precision) + units[number];
     };
   }
 }());
