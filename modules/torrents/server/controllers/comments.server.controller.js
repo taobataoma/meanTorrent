d7adeedcf8b61f64b9264bf0efcc5c611e7cc06a
OldHawk <taobataoma@gmail.com>
2017-04-21 18:29:02 +0800

    add comments server controller

---
+++ modules/torrents/server/controllers/comments.server.controller.js
@@ -0,0 +1,82 @@
+'use strict';
+
+/**
+ * Module dependencies
+ */
+var path = require('path'),
+  config = require(path.resolve('./config/config')),
+  mongoose = require('mongoose'),
+  errorHandler = require(path.resolve('./modules/core/server/controllers/errors.server.controller'));
+
+/**
+ * create a comment of torrent
+ * @param req
+ * @param res
+ */
+exports.create = function (req, res) {
+
+};
+
+/**
+ * update a comment of torrent
+ * @param req
+ * @param res
+ */
+exports.update = function (req, res) {
+
+};
+
+/**
+ * delete a comment of torrent
+ * @param req
+ * @param res
+ */
+exports.delete = function (req, res) {
+
+};
+
+/**
+ * list all comment of torrent
+ * @param req
+ * @param res
+ */
+exports.list = function (req, res) {
+
+};
+
+/**
+ * create a sub comment of comment
+ * @param req
+ * @param res
+ */
+exports.SubCreate = function (req, res) {
+
+};
+
+/**
+ * update a sub comment of comment
+ * @param req
+ * @param res
+ */
+exports.SubUpdate = function (req, res) {
+
+};
+
+/**
+ * delete a sub comment of comment
+ * @param req
+ * @param res
+ */
+exports.SubDelete = function (req, res) {
+
+};
+
+/**
+ * list all sub comment of comment
+ * @param req
+ * @param res
+ */
+exports.SubList = function (req, res) {
+
+};
+
