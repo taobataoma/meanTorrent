cd115df1f269fd6f95bcc47136e5f9fad1a09691
OldHawk <taobataoma@gmail.com>
2017-04-25 10:55:53 +0800

    add subtitle upload multer config

--- config/lib/multer.js
+++ config/lib/multer.js
@@ -39,3 +39,20 @@ module.exports.createUploadFilename = function (req, file, cb) {
 module.exports.getUploadDestination = function (req, file, cb) {
   cb(null, config.uploads.torrent.file.dest);
 };
+
+module.exports.createUploadSubtitleFilename = function (req, file, cb) {
+  var regex = new RegExp(',', 'g');
+  var filename = file.originalname.replace(regex, ' ');
+
+  if (fs.existsSync(config.uploads.subtitles.file.dest + filename)) {
+    var err = new Error();
+    err.code = 'FILE_ALREADY_EXISTS';
+    cb(err, null);
+  } else {
+    cb(null, filename);
+  }
+};
+
+module.exports.getUploadSubtitleDestination = function (req, file, cb) {
+  cb(null, config.uploads.subtitles.file.dest);
+};
