0c6606bc35436393d2f841a7e0834a7fb41bc741
OldHawk <taobataoma@gmail.com>
2017-05-03 12:02:21 +0800

    ui-cropper worked fine!!!

--- modules/users/client/controllers/settings/change-profile-picture.client.controller.js
+++ modules/users/client/controllers/settings/change-profile-picture.client.controller.js
@@ -5,20 +5,38 @@
     .module('users')
     .controller('ChangeProfilePictureController', ChangeProfilePictureController);
 
-  ChangeProfilePictureController.$inject = ['$timeout', 'Authentication', 'Upload', 'Notification'];
+  ChangeProfilePictureController.$inject = ['$scope', '$timeout', 'Authentication', 'Upload', 'Notification'];
 
-  function ChangeProfilePictureController($timeout, Authentication, Upload, Notification) {
+  function ChangeProfilePictureController($scope, $timeout, Authentication, Upload, Notification) {
     var vm = this;
 
     vm.user = Authentication.user;
     vm.progress = 0;
 
-    vm.upload = function (dataUrl) {
+    vm.selectedImageUrl = '';
+    vm.resultImageURL = '';
 
+
+    Upload.onBeforeUploadItem = function (item) {
+      var blob = vm.dataURItoBlob(vm.resultImageURL);
+      item._file = blob;
+    };
+
+    vm.dataURItoBlob = function (dataURI) {
+      var binary = atob(dataURI.split(',')[1]);
+      var mimeString = dataURI.split(',')[0].split(':')[1].split(';')[0];
+      var array = [];
+      for (var i = 0; i < binary.length; i++) {
+        array.push(binary.charCodeAt(i));
+      }
+      return new Blob([new Uint8Array(array)], {type: mimeString});
+    };
+
+    vm.upload = function () {
       Upload.upload({
         url: '/api/users/picture',
         data: {
-          newProfilePicture: dataUrl
+          newProfilePicture: vm.dataURItoBlob(vm.resultImageURL)
         }
       }).then(function (response) {
         $timeout(function () {
@@ -29,28 +47,57 @@
       }, function (evt) {
         vm.progress = parseInt(100.0 * evt.loaded / evt.total, 10);
       });
+
+
+      // Called after the user has successfully uploaded a new picture
+      function onSuccessItem(response) {
+        // Show success message
+        Notification.success({message: '<i class="glyphicon glyphicon-ok"></i> Successfully changed profile picture'});
+
+        // Populate user object
+        vm.user = Authentication.user = response;
+
+        // Reset form
+        vm.fileSelected = false;
+        vm.progress = 0;
+        vm.selectedImageUrl = '';
+        vm.resultImageURL = '';
+      }
+
+      // Called after the user has failed to upload a new picture
+      function onErrorItem(response) {
+        vm.fileSelected = false;
+        vm.progress = 0;
+        vm.selectedImageUrl = '';
+        vm.resultImageURL = '';
+
+        // Show error message
+        Notification.error({message: response.message, title: '<i class="glyphicon glyphicon-remove"></i> Failed to change profile picture'});
+      }
     };
 
-    // Called after the user has successfully uploaded a new picture
-    function onSuccessItem(response) {
-      // Show success message
-      Notification.success({ message: '<i class="glyphicon glyphicon-ok"></i> Successfully changed profile picture' });
+    vm.handleFileSelect = function (evt) {
+      vm.loading = false;
 
-      // Populate user object
-      vm.user = Authentication.user = response;
+      if(evt.currentTarget.files) {
+        var file = evt.currentTarget.files[0];
 
-      // Reset form
+        var reader = new FileReader();
+        reader.onload = function (evt) {
+          $scope.$apply(function () {
+            vm.fileSelected = true;
+            vm.selectedImageUrl = evt.target.result;
+          });
+        };
+        reader.readAsDataURL(file);
+      }
+    };
+
+    vm.cancel = function () {
       vm.fileSelected = false;
       vm.progress = 0;
-    }
-
-    // Called after the user has failed to upload a new picture
-    function onErrorItem(response) {
-      vm.fileSelected = false;
-      vm.progress = 0;
-
-      // Show error message
-      Notification.error({ message: response.message, title: '<i class="glyphicon glyphicon-remove"></i> Failed to change profile picture' });
-    }
+      vm.selectedImageUrl = '';
+      vm.resultImageURL = '';
+    };
   }
 }());
