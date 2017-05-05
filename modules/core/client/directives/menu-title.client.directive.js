85f1724db6f99dc09bf89b69514add5405909a74
OldHawk <taobataoma@gmail.com>
2017-05-05 17:22:19 +0800

    add menu-title directive

---
+++ modules/core/client/directives/menu-title.client.directive.js
@@ -0,0 +1,29 @@
+(function () {
+  'use strict';
+
+  angular.module('core')
+    .directive('menuTitle', menuTitle);
+
+  menuTitle.$inject = ['$rootScope', '$interpolate', '$state', '$translate'];
+
+  function menuTitle($rootScope, $interpolate, $state, $translate) {
+    var directive = {
+      restrict: 'A',
+      link: link
+    };
+
+    return directive;
+
+    function link(scope, element, attrs) {
+      $rootScope.$on('$stateChangeSuccess', listener);
+
+      function listener(event, toState) {
+        var menu_title = attrs.menuTitle;
+        var trans = $translate.instant(menu_title);
+
+        attrs.$set('ng-bind', trans);
+        element.html(trans);
+      }
+    }
+  }
+}());
