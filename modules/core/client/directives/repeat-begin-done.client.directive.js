c2b952c6a5baa25d64bcf53315c910741e11567c
OldHawk <taobataoma@gmail.com>
2017-05-10 17:52:16 +0800

    add ng-repeat listen event repeat-begin and repeat-done

--- modules/core/client/directives/repeat-begin-done.client.directive.js
+++ modules/core/client/directives/repeat-begin-done.client.directive.js
@@ -2,6 +2,26 @@
   'use strict';
 
   angular.module('core')
+    .directive('repeatBegin', repeatBegin);
+
+  function repeatBegin() {
+    var directive = {
+      restrict: 'A',
+      link: link
+    };
+
+    return directive;
+
+    function link(scope, element, attrs) {
+      if (scope.$first) {
+        if (attrs.repeatBegin) {
+          scope.$eval(attrs.repeatBegin);
+        }
+      }
+    }
+  }
+
+  angular.module('core')
     .directive('repeatDone', repeatDone);
 
   function repeatDone() {
