e416c78160b6aa83665da914eef683d92e26ebba
OldHawk <taobataoma@gmail.com>
2017-06-16 15:58:30 +0800

    feat(messages): add message model

---
+++ modules/messages/server/models/message.server.model.js
@@ -0,0 +1,51 @@
+'use strict';
+
+/**
+ * Module dependencies
+ */
+var mongoose = require('mongoose'),
+  Schema = mongoose.Schema;
+
+/**
+ * Message Schema
+ */
+var MessageSchema = new Schema({
+  from_user: {
+    type: Schema.Types.ObjectId,
+    ref: 'User'
+  },
+  to_user: {
+    type: Schema.Types.ObjectId,
+    ref: 'User'
+  },
+  title: {
+    type: String,
+    default: '',
+    trim: true
+  },
+  content: {
+    type: String,
+    default: '',
+    trim: true
+  },
+  type: {
+    type: String,
+    default: 'user'
+  },
+  _replies: [this],
+
+  from_status: {
+    type: Number,
+    default: 0      //0 unread, 1 already read
+  },
+  to_status: {
+    type: Number,
+    default: 0      //0 unread, 1 already read
+  },
+  createdat: {
+    type: Date,
+    default: Date.now
+  }});
+
+
+mongoose.model('Message', MessageSchema);
