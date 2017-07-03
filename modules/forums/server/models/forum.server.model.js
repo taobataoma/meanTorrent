241b86bc24219a210181f0189432ba2c5f0d3be2
OldHawk <taobataoma@gmail.com>
2017-07-03 13:48:32 +0800

    feat(forums): add forum server model

---
+++ modules/forums/server/models/forum.server.model.js
@@ -0,0 +1,67 @@
+'use strict';
+
+/**
+ * Module dependencies
+ */
+var mongoose = require('mongoose'),
+  Schema = mongoose.Schema;
+
+/**
+ * Forum Schema
+ */
+var ForumSchema = new Schema({
+  name: {
+    type: String,
+    default: '',
+    trim: true
+  },
+  desc: {
+    type: String,
+    default: '',
+    trim: true
+  },
+  img: {
+    type: String,
+    default: '',
+    trim: true
+  },
+  order: {
+    type: Number,
+    default: 0
+  },
+  readOnly: {
+    type: Boolean,
+    default: false
+  },
+
+  topicCount: {
+    type: Number,
+    default: 0
+  },
+  replyCount: {
+    type: Number,
+    default: 0
+  },
+  todayCount: {
+    type: Number,
+    default: 0
+  },
+
+  lastTopic: {
+    type: Schema.Types.ObjectId,
+    ref: 'Topic'
+  },
+
+  moderators: [{
+    type: Schema.Types.ObjectId,
+    ref: 'User'
+  }],
+
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+
+mongoose.model('Forum', ForumSchema);
