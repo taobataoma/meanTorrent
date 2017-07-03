a7eb5eef3fef1fe82008c7196468ad0a6b9cd463
OldHawk <taobataoma@gmail.com>
2017-07-03 18:27:07 +0800

    feat(forums): add forums admin list view side-overlay

--- modules/forums/client/controllers/forums.client.controller.js
+++ modules/forums/client/controllers/forums.client.controller.js
@@ -5,18 +5,29 @@
     .module('forums')
     .controller('ForumsController', ForumsController);
 
-  ForumsController.$inject = ['$scope', '$translate', 'Authentication', 'MeanTorrentConfig', 'ForumsAdminService'];
+  ForumsController.$inject = ['$scope', '$translate', 'Authentication', 'MeanTorrentConfig', 'ForumsAdminService', 'SideOverlay'];
 
-  function ForumsController($scope, $translate, Authentication, MeanTorrentConfig, ForumsAdminService) {
+  function ForumsController($scope, $translate, Authentication, MeanTorrentConfig, ForumsAdminService, SideOverlay) {
     var vm = this;
     vm.forumsConfig = MeanTorrentConfig.meanTorrentConfig.forumsConfig;
     vm.user = Authentication.user;
 
+    /**
+     * init
+     */
     vm.init = function () {
       ForumsAdminService.query({}, function (items) {
         vm.forums = items;
         console.log(items);
       });
+    };
+
+    /**
+     * openSideOverlay
+     * @param evt
+     */
+    vm.openSideOverlay = function (evt) {
+      SideOverlay.open(evt, 'popupSlide');
     };
   }
 }());
