9ea721ec290074bd4ef3d12afbfbe996a66fa6ef
OldHawk <taobataoma@gmail.com>
2017-07-04 17:49:11 +0800

    feat(users): forbid user login when banned from server

--- modules/users/server/config/strategies/local.js
+++ modules/users/server/config/strategies/local.js
@@ -10,32 +10,33 @@ var passport = require('passport'),
 module.exports = function () {
   // Use local strategy
   passport.use(new LocalStrategy({
-    usernameField: 'usernameOrEmail',
-    passwordField: 'password'
-  },
-  function (usernameOrEmail, password, done) {
-    User.findOne({
-      $or: [{
-        username: usernameOrEmail.toLowerCase()
-      }, {
-        email: usernameOrEmail.toLowerCase()
-      }]
-    }, function (err, user) {
-      if (err) {
-        return done(err);
-      }
-      if (!user || !user.authenticate(password)) {
-        return done(null, false, {
-          message: 'Invalid username or password (' + (new Date()).toLocaleTimeString() + ')'
-        });
-      }
-      if(user.status === 'banned'){
-        return done(null, false, {
-          message: 'You are banned from the server!'
-        });
-      }
+      usernameField: 'usernameOrEmail',
+      passwordField: 'password'
+    },
+    function (usernameOrEmail, password, done) {
+      User.findOne({
+        $or: [{
+          username: usernameOrEmail.toLowerCase()
+        }, {
+          email: usernameOrEmail.toLowerCase()
+        }]
+      }, function (err, user) {
+        if (err) {
+          return done(err);
+        }
+        if (!user || !user.authenticate(password)) {
+          return done(null, false, {
+            message: 'Invalid username or password (' + (new Date()).toLocaleTimeString() + ')'
+          });
+        }
+        if (user.status === 'banned') {
+          return done(null, false, {
+            message: 'You are banned from the server!'
+          });
+        }
 
-      return done(null, user);
-    });
-  }));
+        return done(null, user);
+      });
+    }
+  ));
 };
