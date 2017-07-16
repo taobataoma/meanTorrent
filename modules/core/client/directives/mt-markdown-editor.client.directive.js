28f20af9dce742f6ece748c4f6d64da752b01df0
OldHawk <taobataoma@gmail.com>
2017-07-16 16:42:28 +0800

    feat(forums): attach file upload add manual-file-chooser.

--- modules/core/client/directives/mt-markdown-editor.client.directive.js
+++ modules/core/client/directives/mt-markdown-editor.client.directive.js
@@ -33,7 +33,7 @@
           scope.uFiles = [];
           scope.uImages = [];
 
-          var eleUploadTip = angular.element('<div class="attach-info" ng-show="!uFile"><div class="attach-upload-tooltip text-long"><span>{{\'FORUMS.ATTACH_UPLOAD_TOOLTIP\' | translate}}</span></div></div>');
+          var eleUploadTip = angular.element('<div class="attach-info" ng-show="!uFile"><div class="attach-upload-tooltip text-long"><span>{{\'FORUMS.ATTACH_UPLOAD_TOOLTIP1\' | translate}}</span><input type="file" class="manual-file-chooser" ng-model="selectedFile" ngf-select="onFileSelected($event);"><span class="btn-link manual-file-chooser-text">{{\'FORUMS.ATTACH_UPLOAD_TOOLTIP2\' | translate}}</span>{{\'FORUMS.ATTACH_UPLOAD_TOOLTIP3\' | translate}}</div></div>');
           var eleUploadBegin = angular.element('<div class="upload-info" ng-show="uFile"><i class="fa fa-cog fa-spin fa-lg fa-fw"></i> <div class="attach-upload-progress" style="width: {{uProgress}}%"></div><div class="attach-upload-filename">{{\'FORUMS.ATTACH_UPLOADING\' | translate}}: {{uFile.name}}</div></div>');
           var eleUploadList = angular.element('<div class="attach-list" ng-show="uFiles.length"><div><ol><li ng-repeat="f in uFiles track by $index">{{f.name}}　<i class="fa fa-times" ng-click="removeAttach($index)"></i></li></ol></div></div>');
 
@@ -49,6 +49,10 @@
             scope.uFiles.splice(idx, 1);
           };
 
+          scope.onFileSelected = function(evt){
+            doUpload(scope.selectedFile);
+          };
+
           $('.md-editor').bind('dragenter', function (evt) {
             evt.stopPropagation();
             evt.preventDefault();
@@ -62,8 +66,13 @@
             evt.stopPropagation();
             evt.preventDefault();
 
+            doUpload(evt.originalEvent.dataTransfer.files[0]);
+            return false;
+          });
+
+          function doUpload(sFile){
             if (attrs.uploadMethod) {
-              scope.uFile = evt.originalEvent.dataTransfer.files[0];
+              scope.uFile = sFile;
               scope.uProgress = 0;
 
               scope.$eval(attrs.uploadMethod, {
@@ -99,9 +108,7 @@
                 }
               });
             }
-            return false;
-          });
-
+          }
           $compile($('.md-editor').contents())(scope);
         }
       });
