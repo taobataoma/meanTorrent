bf791ce8c51ddf81c4070974290354657ad1315a
OldHawk <taobataoma@gmail.com>
2017-07-17 22:16:42 +0800

    feat(users): add user-info client directive

---
+++ modules/users/client/directives/user-info.client.directive.js
@@ -0,0 +1,29 @@
+(function () {
+  'use strict';
+
+  angular.module('users')
+    .directive('userInfo', userInfo);
+
+  userInfo.$inject = ['$compile', '$translate'];
+
+  function userInfo($compile, $translate) {
+    var directive = {
+      restrict: 'A',
+      link: link
+    };
+
+    return directive;
+
+    function link(scope, element, attrs) {
+      scope.$watch(attrs.userInfo, function (s) {
+        if (s) {
+          var user = s;
+          var title = $translate.instant('USER_INFO_TITLE', {name: user.displayName});
+          var e = angular.element('<a href="#" ui-sref="userinfo({userId: \'' + user._id + '\'})" title="' + title + '">' + element[0].innerHTML + '</a>');
+          element.html(e[0].outerHTML);
+          $compile(element.contents())(scope);
+        }
+      });
+    }
+  }
+}());
