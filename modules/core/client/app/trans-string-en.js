f61f2fc3e5fba26994b690f92157849a8a989c06
OldHawk <taobataoma@gmail.com>
2017-07-21 14:56:01 +0800

    fix(chat): thumbs-up translate string

--- modules/core/client/app/trans-string-en.js
+++ modules/core/client/app/trans-string-en.js
@@ -741,7 +741,7 @@
         TOPIC_DELETE: 'delete topic',
         TOPIC_TOP_TOPIC: 'top status',
         TOPIC_GLOBAL_TOPIC: 'global status',
-        TOPIC_THUMBS_UP: 'thumbs up and donate score',
+        TOPIC_THUMBS_UP: 'thumbs up and donate score ({{number}})',
         HAS_ATTACH: 'has attach files'
       }
     },
--- modules/core/client/app/trans-string-zh.js
+++ modules/core/client/app/trans-string-zh.js
@@ -741,7 +741,7 @@
         TOPIC_DELETE: '删除话题',
         TOPIC_TOP_TOPIC: '置顶话题',
         TOPIC_GLOBAL_TOPIC: '全局话题',
-        TOPIC_THUMBS_UP: '点赞送分',
+        TOPIC_THUMBS_UP: '点赞并赠送积分 ({{number}})',
         HAS_ATTACH: '包含附件'
       }
     },
