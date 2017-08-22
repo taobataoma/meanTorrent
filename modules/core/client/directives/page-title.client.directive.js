a2f149da90001e7dc2aea092e5b9d9a162a19620
OldHawk <taobataoma@gmail.com>
2017-08-22 13:48:04 +0800

    fix(core): restore page-title directive

--- modules/core/client/directives/page-title.client.directive.js
+++ modules/core/client/directives/page-title.client.directive.js
@@ -19,17 +19,13 @@
 
       function listener(event, toState) {
         var applicationCoreTitle = 'CHD.im',
-          separator = ' - ',
-          stateTitle = applicationCoreTitle + separator;
-
-        toState.name.split('.').forEach(function (value, index) {
-          stateTitle = stateTitle + value.charAt(0).toUpperCase() + value.slice(1) + separator;
-        });
+          separeteBy = ' - ';
         if (toState.data && toState.data.pageTitle) {
-          stateTitle = $interpolate(stateTitle + $translate.instant(toState.data.pageTitle) + separator)(($state.$current.locals.globals));
+          var stateTitle = $interpolate(toState.data.pageTitle)($state.$current.locals.globals);
+          element.html(applicationCoreTitle + separeteBy + $translate.instant(stateTitle));
+        } else {
+          element.html(applicationCoreTitle);
         }
-        stateTitle = stateTitle.slice(0, 0 - separator.length);
-        element.text(stateTitle);
       }
     }
   }
