69b95c59b9d2e81c29fd5c93d4883caf6c8335b5
OldHawk <taobataoma@gmail.com>
2017-09-07 17:07:43 +0800

    feat(torrent): add 'save' & 'update' hook of table 'complete'

--- modules/torrents/server/models/complete.server.model.js
+++ modules/torrents/server/models/complete.server.model.js
@@ -42,11 +42,48 @@ var CompleteSchema = new Schema({
     type: Boolean,
     default: false
   },
+  remove_warning_score: {
+    type: Number,
+    default: 0
+  },
+  remove_warning_at: {
+    type: Date,
+    default: ''
+  },
   createdAt: {
     type: Date,
     default: Date.now
   }
 });
+
+CompleteSchema.pre('save', function (next) {
+  countRatio(this);
+  countSeedDay(this);
+  next();
+});
+
+/**
+ * Hook a pre save method to hash the password
+ */
+CompleteSchema.pre('update', function (next) {
+  countRatio(this);
+  countSeedDay(this);
+  next();
+});
+
+function countRatio(t) {
+  if (t.total_uploaded > 0 && t.total_downloaded === 0) {
+    t.total_ratio = -1;
+  } else if (t.total_uploaded === 0 || t.total_downloaded === 0) {
+    t.total_ratio = 0;
+  } else {
+    t.total_ratio = Math.round((t.total_uploaded / t.total_downloaded) * 100) / 100;
+  }
+}
+
+function countSeedDay(t) {
+  t.total_seed_day = Math.floor(t.total_seed_time / (60 * 60 * 1000 * 24));
+}
 
 CompleteSchema.index({user: -1, createdAt: -1});
 CompleteSchema.index({torrent: 1, createdAt: -1});
