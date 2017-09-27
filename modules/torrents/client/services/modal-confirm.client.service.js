07d87463c0b1aabf240aea4ab7c9316d769456bd
OldHawk <taobataoma@gmail.com>
2017-09-27 14:24:49 +0800

    fix(torrents): fixed model-confirm service error in prod env
    Fixes #12

--- modules/torrents/client/services/modal-confirm.client.service.js
+++ modules/torrents/client/services/modal-confirm.client.service.js
@@ -8,13 +8,8 @@
   ModalConfirmService.$inject = ['$uibModal'];
 
   function ModalConfirmService($uibModal) {
-
-    var modalDefaults = {
-      backdrop: true,
-      keyboard: true,
-      modalFade: true,
-      templateUrl: '/modules/torrents/client/templates/modal-confirm.client.view.html'
-    };
+    var cModalOptions = {};
+    var tModalOptions = {};
 
     var modalOptions = {
       closeButtonText: 'Close',
@@ -24,9 +19,27 @@
       bodyParams: undefined
     };
 
+    var modalDefaults = {
+      backdrop: true,
+      keyboard: true,
+      modalFade: true,
+      templateUrl: '/modules/torrents/client/templates/modal-confirm.client.view.html',
+      controller: 'ModalConfirmController',
+      resolve: {
+        modalOptions: function () {
+          //Map modal.html $scope custom properties to defaults defined in service
+          angular.extend(tModalOptions, modalOptions, cModalOptions);
+          return tModalOptions;
+        }
+      }
+    };
+
     var showModal = function (customModalDefaults, customModalOptions) {
       if (!customModalDefaults) customModalDefaults = {};
       customModalDefaults.backdrop = 'static';
+
+      cModalOptions = customModalOptions;
+
       return this.show(customModalDefaults, customModalOptions);
     };
 
@@ -38,21 +51,6 @@
       //Map angular-ui modal custom defaults to modal defaults defined in service
       angular.extend(tempModalDefaults, modalDefaults, customModalDefaults);
 
-      //Map modal.html $scope custom properties to defaults defined in service
-      angular.extend(tempModalOptions, modalOptions, customModalOptions);
-
-      if (!tempModalDefaults.controller) {
-        tempModalDefaults.controller = function ($scope, $uibModalInstance) {
-          $scope.modalOptions = tempModalOptions;
-          $scope.modalOptions.ok = function (result) {
-            $uibModalInstance.close(result);
-          };
-          $scope.modalOptions.close = function (result) {
-            $uibModalInstance.dismiss('cancel');
-          };
-        };
-      }
-
       return $uibModal.open(tempModalDefaults).result;
     };
 
@@ -62,4 +60,22 @@
     };
 
   }
+
+  angular
+    .module('torrents')
+    .controller('ModalConfirmController', ModalConfirmController);
+
+  ModalConfirmController.$inject = ['$scope', '$uibModalInstance', 'modalOptions'];
+
+  function ModalConfirmController($scope, $uibModalInstance, modalOptions) {
+    $scope.modalOptions = modalOptions;
+    $scope.modalOptions.ok = function (result) {
+      $uibModalInstance.close(result);
+    };
+    $scope.modalOptions.close = function (result) {
+      $uibModalInstance.dismiss('cancel');
+    };
+  }
+
 }());
+
