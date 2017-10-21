d4d873ee92d5f55ad27d8e81bebdc6023c94806a
OldHawk <taobataoma@gmail.com>
2017-10-22 01:34:08 +0800

    cfg(dev): config test mail option

--- config/env/production.js
+++ config/env/production.js
@@ -79,12 +79,12 @@ module.exports = {
     sandbox: false
   },
   mailer: {
-    from: process.env.MAILER_FROM || 'MAILER_FROM',
+    from: process.env.MAILER_FROM || 'admin@chd.im',
     options: {
-      service: process.env.MAILER_SERVICE_PROVIDER || 'MAILER_SERVICE_PROVIDER',
+      service: process.env.MAILER_SERVICE_PROVIDER || '"Gmail"',
       auth: {
-        user: process.env.MAILER_EMAIL_ID || 'MAILER_EMAIL_ID',
-        pass: process.env.MAILER_PASSWORD || 'MAILER_PASSWORD'
+        user: process.env.MAILER_EMAIL_ID || 'taobataoma@gmail.com',
+        pass: process.env.MAILER_PASSWORD || 'ykyghbmpjfdliyph'
       }
     }
   },
