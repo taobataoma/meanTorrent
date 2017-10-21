fdea860a5794762d95f82e1643329b93d6df0419
OldHawk <taobataoma@gmail.com>
2017-10-22 01:51:20 +0800

    fix(users): fix esslint warning

--- modules/invitations/server/controllers/invitations.server.controller.js
+++ modules/invitations/server/controllers/invitations.server.controller.js
@@ -302,7 +302,7 @@ exports.sendOfficial = function (req, res) {
           if (config.secure && config.secure.ssl === true) {
             httpTransport = 'https://';
           }
-          var baseUrl = req.app.get('domain') || config.domain ||  httpTransport + req.headers.host;
+          var baseUrl = req.app.get('domain') || config.domain || httpTransport + req.headers.host;
           res.render(path.resolve('modules/invitations/server/templates/invite-sign-up-email'), {
             to_email: req.body.email,
             name: req.user.displayName,
