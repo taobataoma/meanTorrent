98cdc94e2bc568560f3510330d5c58dee55bc051
OldHawk <taobataoma@gmail.com>
2017-11-03 16:53:09 +0800

    feat(torrents): show seed or leech progress stats in home torrent list

--- modules/core/client/directives/torrent-progress.client.directive.js
+++ modules/core/client/directives/torrent-progress.client.directive.js
@@ -22,12 +22,12 @@
       scope.$watch(attrs.torrentProgress, function (p) {
         if (p && p.length > 0) {
           var pt = p[0];
-          mtDebug.info(pt);
+          //mtDebug.info(pt);
 
           var t_progressbar = ngProgressFactory.createInstance();
           t_progressbar.setParent(element[0]);
           t_progressbar.setAbsolute();
-          t_progressbar.setHeight('3px');
+          t_progressbar.setHeight('2px');
           if(pt.peer_status === PEERSTATE_SEEDER){
             t_progressbar.set(100);
             t_progressbar.setColor('#ff6600');
@@ -36,12 +36,59 @@
             t_progressbar.setColor('#5cb85c');
           }
           t_progressbar.progressbarEl.css('background-color', '#dedede');
-          t_progressbar.progressbarEl.css('top', '-6px');
+          t_progressbar.progressbarEl.css('top', '-5px');
           t_progressbar.progressbarEl.css('left', '8px');
           t_progressbar.progressbarEl.css('right', '8px');
 
           //element.css('margin-bottom', '8px');
-          console.log(t_progressbar);
+          //console.log(t_progressbar);
+        }
+      });
+    }
+  }
+
+
+  angular.module('core')
+    .directive('cardProgress', cardProgress);
+
+  cardProgress.$inject = ['$compile', 'DebugConsoleService', 'ngProgressFactory'];
+
+  function cardProgress($compile, mtDebug, ngProgressFactory) {
+    const PEERSTATE_SEEDER = 'seeder';
+    const PEERSTATE_LEECHER = 'leecher';
+
+    var directive = {
+      restrict: 'A',
+      replace: true,
+      link: link
+    };
+
+    return directive;
+
+    function link(scope, element, attrs) {
+      scope.$watch(attrs.cardProgress, function (p) {
+        if (p && p.length > 0) {
+          var pt = p[0];
+          //mtDebug.info(pt);
+
+          var t_progressbar = ngProgressFactory.createInstance();
+          t_progressbar.setParent(element[0]);
+          t_progressbar.setAbsolute();
+          t_progressbar.setHeight('2px');
+          if(pt.peer_status === PEERSTATE_SEEDER){
+            t_progressbar.set(100);
+            t_progressbar.setColor('#ff6600');
+          }else {
+            t_progressbar.set(pt.peer_percent);
+            t_progressbar.setColor('#5cb85c');
+          }
+          t_progressbar.progressbarEl.css('background-color', '#dedede');
+          t_progressbar.progressbarEl.css('top', '0');
+          //t_progressbar.progressbarEl.css('left', '8px');
+          //t_progressbar.progressbarEl.css('right', '8px');
+
+          //element.css('margin-bottom', '8px');
+          //console.log(t_progressbar);
         }
       });
     }
