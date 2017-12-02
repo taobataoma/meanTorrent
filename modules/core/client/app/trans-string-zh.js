d48f081d376bc63b2e1434e6a525c322d4fe5f3f
OldHawk <taobataoma@gmail.com>
2017-12-02 20:12:45 +0800

    fix(string): fixed translate string.

--- modules/core/client/app/trans-string-zh.js
+++ modules/core/client/app/trans-string-zh.js
@@ -453,7 +453,7 @@
     //admin views
     USERS_LIST: '用户列表',
     USERS_UPDATE: '修改',
-    TORRENT_TYPE_OPTION_NEWEST: '所有示审核的种子',
+    TORRENT_TYPE_OPTION_NEWEST: '所有未审核的种子',
     TORRENT_STATUS_NEW: '未审核',
     TORRENT_STATUS_REVIEWED: '已审核',
     TORRENT_STATUS_DELETED: '已删除',
