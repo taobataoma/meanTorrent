13753d312b872cbb5f1a9dacc8a1d1aceb3ca19d
OldHawk <taobataoma@gmail.com>
2017-12-03 12:55:31 +0800

    fix(torrents): remove torrent tags if not exist in resources tags list

--- modules/torrents/client/services/resources-tags.client.service.js
+++ modules/torrents/client/services/resources-tags.client.service.js
@@ -13,11 +13,17 @@
     var resourcesTags = MeanTorrentConfig.meanTorrentConfig.resourcesTags;
 
     var service = {
-      getTagTitle: getTagTitle
+      getTagTitle: getTagTitle,
+      tagInList: tagInList
     };
 
     return service;
 
+    /**
+     * getTagTitle
+     * @param tag
+     * @returns {string}
+     */
     function getTagTitle(tag) {
       var tmp = tag.toUpperCase();
       var find = false;
@@ -41,6 +47,34 @@
         });
       }
       return tmp;
+    }
+
+    /**
+     * tagInList
+     * @param tag
+     * @returns {string}
+     */
+    function tagInList(tag) {
+      var find = false;
+
+      angular.forEach(resourcesTags.radio, function (item) {
+        angular.forEach(item.value, function (sitem) {
+          if (sitem.name.toUpperCase() === tag.toUpperCase()) {
+            find = true;
+          }
+        });
+      });
+
+      if (!find) {
+        angular.forEach(resourcesTags.checkbox, function (item) {
+          angular.forEach(item.value, function (sitem) {
+            if (sitem.name.toUpperCase() === tag.toUpperCase()) {
+              find = true;
+            }
+          });
+        });
+      }
+      return find;
     }
   }
 }());
