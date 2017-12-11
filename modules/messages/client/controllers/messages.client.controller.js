1a50517e202ea8d3f673c286db629a0e72db3393
OldHawk <taobataoma@gmail.com>
2017-12-11 14:05:16 +0800

    feat(torrents): emoji and markdown richTextbox user message box
    #20

--- modules/messages/client/controllers/messages.client.controller.js
+++ modules/messages/client/controllers/messages.client.controller.js
@@ -17,6 +17,60 @@
     vm.deleteList = [];
 
     /**
+     * initComplete
+     */
+    vm.initComplete = function () {
+      jQuery.fn.simulateESC = function () {
+        console.log('simulateESC');
+        // Internally calls jQuery.event.trigger with arguments (Event, data, elem).
+        // That last argument, 'elem', is very important!
+        jQuery(this).trigger({type: 'keypress', which: 27});
+      };
+      $('.message-textarea').textcomplete([
+        { // emoji strategy
+          match: /\B:([\-+\w]*)$/,
+          search: function (term, callback) {
+            callback($.map(window.emojies, function (emoji) {
+              return emoji.indexOf(term) === 0 ? emoji : null;
+            }));
+          },
+          template: function (value) {
+            return '<img class="ac-emoji" src="/graphics/emojis/' + value + '.png" />' + '<span class="ac-emoji-text">' + value + '</span>';
+          },
+          replace: function (value) {
+            return ':' + value + ': ';
+          },
+          index: 1
+        }
+      ]);
+    };
+
+    /**
+     * initReplyComplete
+     */
+    vm.initReplyComplete = function () {
+      $('.reply-textarea').textcomplete([
+        { // emoji strategy
+          match: /\B:([\-+\w]*)$/,
+          search: function (term, callback) {
+            callback($.map(window.emojies, function (emoji) {
+              return emoji.indexOf(term) === 0 ? emoji : null;
+            }));
+          },
+          template: function (value) {
+            return '<img class="ac-emoji" src="/graphics/emojis/' + value + '.png" />' + '<span class="ac-emoji-text">' + value + '</span>';
+          },
+          replace: function (value) {
+            return ':' + value + ': ';
+          },
+          index: 1
+        }
+      ], {
+        dropdownClassName: 'dropdown-menu textcomplete-dropdown textcomplete-dropup'
+      });
+    };
+
+    /**
      * user-unread-count-changed
      */
     $scope.$on('user-unread-count-changed', function (event, args) {
@@ -183,7 +237,15 @@
         SideOverlay.open(evt, 'popupSlide');
       }
 
+      vm.replyContent = undefined;
       vm.updateReadStatus(msg);
+    };
+
+    /**
+     * onPopupMessageOpen
+     */
+    vm.onPopupMessageOpen = function () {
+      $('.reply-textarea').focus();
     };
 
     /**
@@ -192,6 +254,26 @@
     vm.hideMessage = function () {
       vm.selectedMessage = undefined;
       SideOverlay.close(null, 'popupSlide');
+    };
+
+    /**
+     * onReplyKeyDown
+     * @param e
+     */
+    vm.onReplyKeyDown = function (e) {
+      console.log('onReplyKeyDown: ' + e.keyCode);
+      if (e.keyCode === 27) { // ESC
+        var hasPopupMenu = false;
+        var emojiMenu = $('.textcomplete-dropup');
+        angular.forEach(emojiMenu, function (e) {
+          if (e.style.display === 'block') {
+            hasPopupMenu = true;
+          }
+        });
+        if (hasPopupMenu) {
+          e.stopPropagation();
+        }
+      }
     };
 
     /**
