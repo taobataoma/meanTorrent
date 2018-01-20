292303cbe3fd8b105bdd41fa72efe9da67bdc81d
OldHawk <taobataoma@gmail.com>
2018-01-20 08:16:43 +0800

    fix(core): move textcomplete dropdown menu

--- modules/core/client/config/core.client.route-filter.js
+++ modules/core/client/config/core.client.route-filter.js
@@ -13,6 +13,7 @@
 
     function stateChangeStart(event, toState, toParams, fromState, fromParams) {
       $('.side-background').remove();
+      $('.textcomplete-dropdown').remove();
       // Check authentication before changing state
       if (toState.data && toState.data.roles && toState.data.roles.length > 0) {
         var allowed = false;
