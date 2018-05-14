71631fba2c1b8614c2080021a6102350e1743913
OldHawk <taobataoma@gmail.com>
2018-05-14 15:10:40 +0800

    feat(check): rename module check

--- modules/checkin/client/checkin.client.module.js
+++
@@ -1,9 +0,0 @@
-(function (app) {
-  'use strict';
-
-  app.registerModule('checkin', ['core']);// The core module is required for special route handling; see /core/client/config/core.client.routes
-  app.registerModule('checkin.admin', ['core.admin']);
-  app.registerModule('checkin.admin.routes', ['core.admin.routes']);
-  app.registerModule('checkin.services');
-  app.registerModule('checkin.routes', ['ui.router', 'core.routes', 'checkin.services']);
-}(ApplicationConfiguration));
