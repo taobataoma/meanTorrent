677ecd85dbe1b186b48cd623108ee6c195b9e36b
OldHawk <taobataoma@gmail.com>
2018-06-23 17:13:08 +0800

    fix(medals): fixed medal count number issue

--- modules/users/client/directives/medal-flag.client.directive.js
+++ modules/users/client/directives/medal-flag.client.directive.js
@@ -7,29 +7,33 @@
   medalFlag.$inject = ['$compile', '$translate', '$filter'];
 
   function medalFlag($compile, $translate, $filter) {
+    var user = undefined;
+
     var directive = {
       restrict: 'A',
+      replace: true,
+      scope: {
+        medalUser: '=medalFlag',
+        medalCount: '=medalCount'
+      },
       link: link
     };
 
     return directive;
 
     function link(scope, element, attrs) {
-      scope.$watch(attrs.medalFlag, function (s) {
-        if (s) {
-          var user = s;
-          var number = attrs.medalCount || 0;
-          var title = $translate.instant('USER_MEDAL_FLAG', {name: user.displayName});
-          var cls = attrs.medalFlagClass;
+      scope.$watch(function () {
+        var number = scope.medalCount || 0;
+        var title = $translate.instant('USER_MEDAL_FLAG', {name: scope.medalUser.displayName});
+        var cls = attrs.medalFlagClass;
 
-          var ele = angular.element('<i class="far fa-shield-alt text-mt" ng-click="$event.stopPropagation();"> <span class="medal-data">' + number + '</span></i>');
+        var ele = angular.element('<i class="far fa-shield-alt text-mt" ng-click="$event.stopPropagation();"> <span class="medal-data">' + number + '</span></i>');
 
-          ele.addClass(cls ? cls : '');
-          ele.attr('title', title);
+        ele.addClass(cls ? cls : '');
+        ele.attr('title', title);
 
-          element.append(ele);
-          $compile(element.contents())(scope);
-        }
+        element.html(ele);
+        $compile(element.contents())(scope);
       });
     }
   }
